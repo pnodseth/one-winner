d21d78327042ef3fdb458c3b1c4e964a7bb7a614
Pål Nødseth <pal.nodseth@eggsdesign.com>
2020-04-18 21:05:58 +0200

    progress

--- app.js
+++ app.js
@@ -2,55 +2,137 @@ import Movable from "./movable.js";
 import Infectable from "./InfectableMovable.js";
 import Person from "./Person.js";
 const canvas = document.getElementById("canvas");
-
+const placeSound = new Audio("./assets/sounds/test.mp3");
+const placeSound2 = new Audio("./assets/sounds/test.mp3");
+const placeSound3 = new Audio("./assets/sounds/test.mp3");
+let gameLoopInterval = null;
 start(canvas);
 
 function start(canvas) {
   const allCollidables = [];
+  const losingPlayers = [];
   const canvasWidth = canvas.clientWidth;
   const canvasHeight = canvas.clientHeight;
   const ctx = canvas.getContext("2d");
   ctx.lineWidth = 1;
 
-  const people = [{ name: "Pål" }, { name: "Carl" }, { name: "Bendik" }, { name: "Rolf" }, { name: "Hege" }];
+  const people = [
+    { name: "Pål", type: "person" },
+    { name: "Carl", type: "person" },
+    { name: "Bendik", type: "person" },
+    { name: "Rolf", type: "person" },
+    { name: "Hege", type: "person" },
+    { name: "Ingvild", type: "person" },
+    { name: "Espen", type: "person" },
+    { name: "Håvard", type: "person" },
+    { name: "Helle", type: "person" },
+    { name: "Elisabeth", type: "person" },
+    { name: "Lucas", type: "person" },
+    { name: "Kim", type: "person" },
+    { name: "Hanne", type: "person" },
+    { name: "Maren", type: "person" },
+    { name: "Hanne L", type: "person" },
+    { name: "Martin", type: "person" },
+    { name: "C.G", type: "person" },
+    { name: "Peter", type: "person" },
+    { name: "Mads", type: "person" },
+    { name: "Kevin", type: "person" },
+    { name: "Cathrine", type: "person" },
+    { name: "Sunneva", type: "person" },
+    { name: "Demah", type: "person" },
+    { name: "Valeria", type: "person" },
+    { name: "Sanja", type: "person" },
+    { name: "Kristin", type: "person" }
+  ];
 
   for (let n of people) {
-    let movable = generateMovable({ name: n.name, isInfected: false, isMovable: true, width: canvasWidth, height: canvasHeight, ctx, allCollidables });
+    let movable = generateMovable({
+      name: n.name,
+      isInfected: false,
+      isMovable: true,
+      width: canvasWidth,
+      height: canvasHeight,
+      ctx,
+      allCollidables,
+      type: n.type
+    });
     allCollidables.push(movable);
-    movable.init();
   }
 
-  /* for (let n of new Array(2)) {
-    let movable = generateMovable({ isInfected: true, isMovable: false, width: canvasWidth, height: canvasHeight, ctx, allCollidables, isInfected: true });
-    allCollidables.push(movable);
-    movable.init();
-  } */
-
-  /*   for (let n of new Array(20)) {
-    let movable = generateMovable({ isMovable: false, width: canvasWidth, height: canvasHeight, ctx });
-    allCollidables.push(movable);
-    movable.init();
-  } */
-
-  /* Infected one */
-  /*   let movable = generateMovable({ isMovable: false, width: canvasWidth, height: canvasHeight, ctx, isInfected: true });
-  allCollidables.push(movable);
-  movable.init(); */
-
   /* ANIMATE */
-  setInterval(() => {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    for (let i = 0; i < allCollidables.length; i++) {
-      let movable = allCollidables[i];
-      movable.animate();
+  async function placePlayerOnBoard(p) {
+    return new Promise((res) => {
+      setTimeout(() => {
+        if (placeSound.paused) {
+          placeSound.play();
+        } else if (placeSound2.paused) {
+          placeSound2.play();
+        } else if (placeSound3.paused) {
+          placeSound3.play();
+        }
+        p.draw();
+        res();
+      }, 250);
+    });
+  }
+
+  async function placePlayersOnBoard() {
+    for (let p of allCollidables) {
+      await placePlayerOnBoard(p);
     }
-  }, 1);
+  }
+  window.placePlayersOnBoard = placePlayersOnBoard;
+
+  window.startGame = function () {
+    gameLoopInterval = setInterval(() => {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+      /* Check if game has ended */
+      if (allCollidables.length === 1) {
+        gameEnded(allCollidables, losingPlayers);
+      }
+
+      /* Remove players with no lives left */
+      for (let p = 0; p < allCollidables.length; p++) {
+        if (allCollidables[p].lives === 0) {
+          let losingPlayer = allCollidables.splice(p, 1);
+          losingPlayers.push(losingPlayer);
+        }
+      }
+
+      for (let i = 0; i < allCollidables.length; i++) {
+        if (allCollidables[i].isMovable) {
+          allCollidables[i].update();
+        }
+        allCollidables[i].checkCollisions();
+        allCollidables[i].draw();
+      }
+    }, 1);
+  };
 }
 
+const gameEnded = (allCollidables, losingPlayers) => {
+  clearInterval(gameLoopInterval);
+  console.log("The winner is: ", allCollidables[0].name);
+
+  let all = [...allCollidables, ...losingPlayers];
+  let maxPoints = all.reduce(
+    (a, b) => {
+      if (a.points > b.points) {
+        return a;
+      } else return b;
+    },
+    { points: 0 }
+  );
+
+  console.log("the one with most points: ", maxPoints[0].name);
+};
+
 function generateMovable({ name, isInfected, isMovable = true, width, height, ctx, allCollidables, type }) {
+  let { x, y } = getAvailablePosition(width, height, allCollidables);
   let startPosition = {
-    x: Math.random() * width,
-    y: Math.random() * height + 50
+    x: x,
+    y: y
   };
   let boardMargin = 50;
   if (startPosition.x < boardMargin) startPosition.x = boardMargin;
@@ -63,6 +145,33 @@ function generateMovable({ name, isInfected, isMovable = true, width, height, ct
   };
 
   return new Person({ name, isInfected, ctx, boardSize: { width, height }, startPosition, direction, isMovable, collidables: allCollidables, type });
+}
+
+function getAvailablePosition(width, height, allCollidables) {
+  let x = Math.random() * width;
+  let y = Math.random() * height + 50;
+  let unique = true;
+  for (let i = 0; i < allCollidables.length; i++) {
+    /* let col = allCollidables[i];
+    let squareDistance = (x - col.currentPosition.x) * (x - col.currentPosition.x) + (y - col.currentPosition.y) * (y - col.currentPosition.y);
+    if (squareDistance <= (1000 + col.radius) * (1000 + col.radius)) {
+      unique = false;
+    } */
+    let col = allCollidables[i];
+    if (col.currentPosition.x >= x - 50 && col.currentPosition.x <= x + 50) {
+      if (col.currentPosition.y >= y - 50 && col.currentPosition.y <= y + 50) {
+        unique = false;
+        console.log("overlapping: ", x, col.currentPosition.x, y, col.currentPosition.y);
+        break;
+      }
+    }
+  }
+  if (unique) {
+    return { x, y };
+  } else {
+    console.log("not unique, doing it again");
+    return getAvailablePosition(width, height, allCollidables);
+  }
 }
 
 /* INIT */
